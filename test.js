22118f8d5d3ea7fe6967211c0528046ce9908e77
igsnow <zzy716810918@gmail.com>
2019-10-09 16:30:53 +0800

    滑块拖动

--- test.js
+++ test.js
@@ -26,103 +26,33 @@ const puppeteer = require('puppeteer');
         waitUntil: 'domcontentloaded'
     });
 
+    // 自动填入账号密码
     let accountEl = '.mod-input-loginName input';
     let pwdEl = '.mod-input-password input';
-    // let account = await page.$eval(accountEl, (el, val) => el.value = val, '716810918@qq.com');
-    // console.log(account);
-    // let pwd = await page.$eval(pwdEl, (el, val) => el.value = val, 'gyj388153@');
-    // console.log(pwd);
-
     await page.waitForSelector(accountEl);
     page.type(accountEl, '716810918@qq.com', {delay: 10})
     await page.waitFor(3000);
     await page.waitForSelector(pwdEl);
     page.type(pwdEl, 'gyj388153@', {delay: 10})
+    await page.waitFor(1000);
 
-    await page.waitFor(500);
-    // await page.tap('.mod-login-btn button')
+    // 拖动验证滑块
+    const start = await page.waitForSelector('.nc_iconfont.btn_slide');
+    const startInfo = await start.boundingBox();
 
-    return
+    const end = await page.waitForSelector('.nc-lang-cnt');
+    const endInfo = await end.boundingBox();
 
-    // 监听到导航栏url变化时，当登录成功时跳转到1688详情页
-    if (page.url() === loginUrl) {
-        while (true) {
-            // await page.waitForNavigation({
-            //     // waitUntil: 'load'
-            // })
-            if (page.url() !== loginUrl) {
-                await page.goto(detailUrl, {
-                    // waitUntil: 'load'
-                });
-                break;
-            }
-        }
+    await page.mouse.move(startInfo.x, endInfo.y);
+    await page.mouse.down();
+
+    for (let i = 0; i < endInfo.width; i = i + 5) {
+        await page.mouse.move(startInfo.x + i, endInfo.y);
     }
-
-    // 4、SKU双属性、可展开
-    let firstSku = '白色（001）';
-    let secondSku = '2XL';
-    let num = 5;
-
-    // await page.tap('.sku-variable-img-wrap');
-
-    await page.waitFor(300);
-
-    // 获取商品sku数组
-    const skuObj = await page.$$eval('.table-sku .name', (e, secondSku) => {
-        let arr = [];
-        let second_index = 0;
-        for (let i = 0; i < e.length; i++) {
-            // 如果sku有图片，则sku取span标签的title值
-            if (e[i].children[0].title) {
-                arr.push(e[i].children[0].title)
-                if (e[i].children[0].title == secondSku) {
-                    second_index = i
-                }
-            } else {
-                arr.push(e[i].children[0].innerHTML)
-                if (e[i].children[0].innerHTML == secondSku) {
-                    second_index = i
-                }
-            }
-        }
-        return {arr, second_index};
-    }, secondSku);
-    console.log(skuObj);
-    skuObj.second_index += 1;
-
-    // 数量输入框得聚焦，不然sku下方的价格统计不显示
-    let selector = '.table-sku tr:nth-child(' + skuObj.second_index + ') .amount-input';
-    let up = '.table-sku tr:nth-child(' + skuObj.second_index + ') .amount-up';
-    let down = '.table-sku tr:nth-child(' + skuObj.second_index + ') .amount-down';
-    await page.focus(selector);
-    console.log('=>sku数量输入框已获取焦点,等待输入...');
-
-    await page.waitFor(300);
-
-    // (坑2)自动填写商品数量，但是下方价格不改变，于是先自增一再减一，价格正确显示
-    await page.$eval(selector, (input, num) => input.value = num, num);
-    await page.waitFor(300);
-    await page.tap(up);
-    await page.waitFor(300);
-    await page.tap(down);
-    console.log('=>数量已自动填充完成!');
-
-    // 失去输入框焦点
-    // await page.evaluate((selector) => {
-    //     document.querySelector(selector).blur()
-    // }, selector)
-
-    await page.waitFor(500);
-
-    // 加入购物车
-    await page.tap('.do-cart')
-    console.log('加入购物车成功!')
+    await page.waitFor(3000)
+    await page.mouse.up();
 
 
-    await page.goto('https://' + url, {
-        // waitUntil: 'load'
-    });
     await browser.close();
 
-})()
+})();
