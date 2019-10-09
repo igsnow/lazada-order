a319311c2b5d3c1b2d168df941889834ec21466e
igsnow <zzy716810918@gmail.com>
2019-10-09 16:36:22 +0800

    node服务自动ç滑块登录

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 // {"account":"716810918@qq.com","pwd":"gyj388153@"}
 // {"firstSku":"白色","secondSku":"XXl","num":3}
 
-app.get('/:url/:sku/:pwd', function (req, res, page) {
+app.get('/:sku/:pwd', function (req, res, page) {
     const puppeteer = require('puppeteer');
     const url = req.params.url;
     const sku = req.params.sku;
@@ -33,102 +33,39 @@ app.get('/:url/:sku/:pwd', function (req, res, page) {
         let detailUrl = 'https://www.lazada.com.my/products/100-cotton-cadar-bedsheet-pillow-case-20-colour-i495564195-s904218335.html?';
 
         await page.goto(loginUrl, {
-            // waitUntil: 'load'
+            waitUntil: 'domcontentloaded'
         });
 
-        // 延时，模拟用户操作时长
-        // await page.waitFor(300);
+        // 自动填入账号密码
+        let accountEl = '.mod-input-loginName input';
+        let pwdEl = '.mod-input-password input';
+        await page.waitForSelector(accountEl);
+        page.type(accountEl, '716810918@qq.com', {delay: 10})
+        await page.waitFor(3000);
+        await page.waitForSelector(pwdEl);
+        page.type(pwdEl, 'gyj388153@', {delay: 10})
+        await page.waitFor(1000);
 
-        let accountEL = await page.$('.mod-input-loginName input')
-        await page.waitForSelector(accountEL);
-        page.type(accountEL, 'Hello')
-        res.send('c')
-        return
+        // 拖动验证滑块
+        const start = await page.waitForSelector('.nc_iconfont.btn_slide');
+        const startInfo = await start.boundingBox();
 
-        // 监听到导航栏url变化时，当登录成功时跳转到1688详情页
-        if (page.url() === loginUrl) {
-            while (true) {
-                // await page.waitForNavigation({
-                //     // waitUntil: 'load'
-                // })
-                if (page.url() !== loginUrl) {
-                    await page.goto(detailUrl, {
-                        // waitUntil: 'load'
-                    });
-                    break;
-                }
-            }
+        const end = await page.waitForSelector('.nc-lang-cnt');
+        const endInfo = await end.boundingBox();
+
+        await page.mouse.move(startInfo.x, endInfo.y);
+        await page.mouse.down();
+
+        for (let i = 0; i < endInfo.width; i = i + 5) {
+            await page.mouse.move(startInfo.x + i, endInfo.y);
         }
-
-        return
-
-        // 4、SKU双属性、可展开
-        let firstSku = '白色（001）';
-        let secondSku = '2XL';
-        let num = 5;
-
-        // await page.tap('.sku-variable-img-wrap');
-
-        await page.waitFor(300);
-
-        // 获取商品sku数组
-        const skuObj = await page.$$eval('.table-sku .name', (e, secondSku) => {
-            let arr = [];
-            let second_index = 0;
-            for (let i = 0; i < e.length; i++) {
-                // 如果sku有图片，则sku取span标签的title值
-                if (e[i].children[0].title) {
-                    arr.push(e[i].children[0].title)
-                    if (e[i].children[0].title == secondSku) {
-                        second_index = i
-                    }
-                } else {
-                    arr.push(e[i].children[0].innerHTML)
-                    if (e[i].children[0].innerHTML == secondSku) {
-                        second_index = i
-                    }
-                }
-            }
-            return {arr, second_index};
-        }, secondSku);
-        console.log(skuObj);
-        skuObj.second_index += 1;
-
-        // 数量输入框得聚焦，不然sku下方的价格统计不显示
-        let selector = '.table-sku tr:nth-child(' + skuObj.second_index + ') .amount-input';
-        let up = '.table-sku tr:nth-child(' + skuObj.second_index + ') .amount-up';
-        let down = '.table-sku tr:nth-child(' + skuObj.second_index + ') .amount-down';
-        await page.focus(selector);
-        console.log('=>sku数量输入框已获取焦点,等待输入...');
-
-        await page.waitFor(300);
-
-        // (坑2)自动填写商品数量，但是下方价格不改变，于是先自增一再减一，价格正确显示
-        await page.$eval(selector, (input, num) => input.value = num, num);
-        await page.waitFor(300);
-        await page.tap(up);
-        await page.waitFor(300);
-        await page.tap(down);
-        console.log('=>数量已自动填充完成!');
-
-        // 失去输入框焦点
-        // await page.evaluate((selector) => {
-        //     document.querySelector(selector).blur()
-        // }, selector)
-
-        await page.waitFor(500);
-
-        // 加入购物车
-        await page.tap('.do-cart')
-        console.log('加入购物车成功!')
+        await page.waitFor(3000)
+        await page.mouse.up();
 
 
-        await page.goto('https://' + url, {
-            // waitUntil: 'load'
-        });
-        await browser.close();
+        // await browser.close();
 
-    })()
+    })();
 })
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(1017, () => console.log('Server listening on port 1017!'));
