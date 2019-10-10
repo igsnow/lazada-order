7adac238786ad5489c4568f52b4084edc22512b0
igsnow <zzy716810918@gmail.com>
2019-10-10 16:59:32 +0800

    下标有问题

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 
-let skuStr = '{"Color Family": "Blue", "Size": "5XL", "Quantity": 3}';
+let skuStr = '{"Color Family": "Blue", "Size": "5XL", "Bedding Size": "Single",  "Quantity": 3}';
 let infoStr = '{"account": "716810918@qq.com", "pwd": "gyj388153@"}';
 
 (async () => {
@@ -23,7 +23,7 @@ let infoStr = '{"account": "716810918@qq.com", "pwd": "gyj388153@"}';
     // 先跳转到登录页
     let loginUrl = 'https://member.lazada.com.my/user/login?spm=a2o4k.home.header.d5.1f062e7e5nKtIB&redirect=https%3A%2F%2Fwww.lazada.com.my%2F%3Fspm%3Da2o4k.login_signup.header.dhome.4d3f49fb8YhnCt';
     let detailUrl = 'https://www.lazada.com.my/products/new-plus-size-s-5xl-floral-bomber-jacket-men-hip-hop-slim-fit-flowers-pilot-bomber-jacket-coat-mens-hooded-jackets-i581532837-s1164964719.html?';
-
+    // let detailUrl = 'https://www.lazada.com.my/products/free-shipping-spinarez-spinal-1-kingqueensuper-singlesingle-10-inch-euro-top-foam-padding-coconut-fiber-bonell-spring-hybrid-mattress-tilam-10-years-warranty-i207914898-s259016584.html?spm=a2o4k.home.flashSale.2.1a762e7eDVXu0T&search=1&mp=1&c=fs&clickTrackInfo=%7B%22rs%22%3A%220.49123629950286524%22%2C%22submission_discount%22%3A%2246%25%22%2C%22rmc%22%3A%2244%22%2C%22type%22%3A%22entrance%22%2C%22isw%22%3A%220.3%22%2C%22userid%22%3A%22%22%2C%22sca%22%3A%2225%22%2C%22hourtonow%22%3A%2214%22%2C%22abid%22%3A%22142638%22%2C%22itemid%22%3A%22207914898_0_itself_0.10954014814395574_0.49123629950286524%22%2C%22pvid%22%3A%22910d9565-22fe-4afe-8b7c-1675c279870e%22%2C%22pos%22%3A%220%22%2C%22ccw%22%3A%220.1%22%2C%22rms%22%3A%220.42718446601941745%22%2C%22c2i%22%3A%220.15247942977625034%22%2C%22scm%22%3A%221007.17760.142638.%22%2C%22rmw%22%3A%220.04166714892533479%22%2C%22isrw%22%3A%220.1%22%2C%22rkw%22%3A%220.4%22%2C%22ss%22%3A%220.10954014814395574%22%2C%22i2i%22%3A%220.074%22%2C%22ms%22%3A%220.10954014814395574%22%2C%22itr%22%3A%220.18666666666666668%22%2C%22mt%22%3A%22itself%22%2C%22its%22%3A%22300%22%2C%22promotion_price%22%3A%22375.00%22%2C%22anonid%22%3A%22nfGIZqdRBp7NgyJKAy6Qth4TPM94qLx8%22%2C%22ppw%22%3A%220.0%22%2C%22isc%22%3A%2256%22%2C%22iss2%22%3A%220.49706300393758573%22%2C%22iss1%22%3A%220.01644157369348209%22%2C%22config%22%3A%22%22%7D&scm=1007.17760.142638.0'
     await page.goto(loginUrl, {
         waitUntil: 'domcontentloaded'
     });
@@ -108,7 +108,7 @@ let infoStr = '{"account": "716810918@qq.com", "pwd": "gyj388153@"}';
                 return {index, len}
             }, sizeVal);
             console.log(sizeIdx);
-            await page.$eval('.sku-prop-selection .sku-variable-size:nth-child(' + (sizeIdx.index + 1) + ')', elem => elem.click());
+            await page.$eval('.sku-prop-selection .sku-variable-size:nth-child(' + (sizeIdx.index + 2) + ')', el => el.click());
 
             // await page.$eval('.next-number-picker-handler-up', elem => elem.click());
 
